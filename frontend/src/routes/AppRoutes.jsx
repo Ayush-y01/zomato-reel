dc571a4e39e2ecbd64439bf7a01989eb3fb319b2
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-02-09 20:33:33 +0530

    route handle

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,8 +3,14 @@ import { browserRoouter as Router, Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
     return ( 
-        <>
-        </>
+        <Router>
+            <Routes>
+                <Route path='/user/register' element={<UserRegister />} />
+                <Route path='/user/login' element={<UserLogin />} />
+                <Route path='/food-partner/register' element={<FoodPartnerRegister />} />
+                <Route path='/food-partner/login' element={<FoodPartnerLogin/>} />
+            </Routes>
+        </Router>
      );
 }
 
